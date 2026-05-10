014e0d879a84508f861ac9c496fd6b68e1e41d3d
Kirill Vanin <vanink0811@gmail.com>
2026-05-10 17:58:17 +0300

    Refactor enums TournamentStatus and TaskStatus; clean up role_enum.py; Fix tournament_id field problem in user_role_model

--- api/services/models/user_role_model.py
+++ api/services/models/user_role_model.py
@@ -5,5 +5,5 @@ from enums.role_enum import RoleEnum
 class UserRoleModel(BaseModel):
     id: Optional[int] = None
     user_id: int
-    tournament_id: int
+    tournament_id: Optional[int] = None
     role: RoleEnum
