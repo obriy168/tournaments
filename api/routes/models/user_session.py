fb92754f679b7487c0f50d922b14cd895c8260ac
Skyline 168 <obriy168@gmail.com>
2026-05-11 22:58:20 +0300

    Add roles to me method

--- api/routes/models/user_session.py
+++ api/routes/models/user_session.py
@@ -1,12 +1,9 @@
 from pydantic import BaseModel
+from routes.models.user_role_model import UserRoleModel
 
 class UserSession(BaseModel):
     user_id: int
     email: str
     #first_name: str
     #last_name: str
-    roles: list[UserSessionRole]
-
-class UserSessionRole(BaseModel):
-    tournament_id: int | None
-    role: str
+    roles: list[UserRoleModel]
