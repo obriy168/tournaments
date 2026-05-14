3e3a62bdd1993cd115d1c8f54eaa98b4882b0371
Kirill Vanin <vanink0811@gmail.com>
2026-05-14 17:57:46 +0300

    Little refactor get user by email:)

--- api/routes/users.py
+++ api/routes/users.py
@@ -7,6 +7,7 @@ from enums.role_enum import RoleEnum
 from util.access.role_required import RoleRequired
 from util.auth import validate_session
 from routes.models.user_session import UserSession
+from routes.models.user_team_response import UserTeamResponse
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -18,10 +19,10 @@ async def get_user(user_id: int, users_service: Annotated[UserService, Depends(U
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@user_router.get("/")
+@user_router.get("/email/", response_model=UserTeamResponse)
 async def get_user_by_email(email: str, users_service: Annotated[UserService, Depends(UserService)],
-                            user_session: Annotated[UserSession, Depends(RoleRequired([RoleEnum.ADMIN]))]):
-    return await users_service.get_user_by_email()
+                            user_session: Annotated[UserSession, Depends(validate_session)]):
+    return await users_service.get_user_by_email(email)
 
 @user_router.get("/")
 async def get_all_users(users_service: Annotated[UserService, Depends(UserService)],
